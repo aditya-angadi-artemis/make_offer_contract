7a572ef6f965fdb186d58de65b43ba1fd8c00320
Aditya Angadi <adityaangadi96@gmail.com>
2021-12-02 15:58:49 +0000

    almost_Complete
    Signed-off-by: Aditya Angadi <adityaangadi96@gmail.com>

--- programs/quidproquo/src/lib.rs
+++ programs/quidproquo/src/lib.rs
@@ -4,11 +4,27 @@ use anchor_spl::{
     token::{Mint, Token, TokenAccount},
 };
 
-declare_id!("CMr8vcCZ1pqPmjWL25m213GSGZGKhcpxHMNbHAhyLXjr");
+use metaplex_token_metadata;
+
+declare_id!("HXu9ZCMB6AsL9s3yV76H72T3cZbKc6UxZA4oebaUwGy7");
 
 #[program]
 pub mod quidproquo {
     use super::*;
+
+    pub fn new(
+        ctx: Context<Initialize>,
+        _data_bump: u8,
+        mk_cut: u64,
+        rent_cut: u64
+    ) -> ProgramResult {
+        let data_acc = &mut ctx.accounts.data_acc;
+        data_acc.market_place = ctx.accounts.beneficiary.key();
+        data_acc.rent = ctx.accounts.rent_account.key();
+        data_acc.market_place_cut = mk_cut;
+        data_acc.rent_cut = rent_cut;
+        Ok(())
+    }
 
     // Make a binding offer of `offer_maker_amount` of one kind of tokens in
     // exchange for `offer_taker_amount` of some other kind of tokens. This
@@ -56,11 +72,24 @@ pub mod quidproquo {
         )
     }
 
+
+
     // Accept an offer by providing the right amount + kind of tokens. This
     // unlocks the tokens escrowed by the offer maker.
     pub fn accept(ctx: Context<Accept>, _offer_bump:u8) -> ProgramResult {
         
-    //    msg!("OFFER BUFFER {}", ctx.accounts.offer.key);
+       
+       let mut taker_amount = ctx.accounts.offer.taker_amount;
+       // Multi by 10
+       let market_cut = ctx.accounts.data_acc.market_place_cut * taker_amount / 1000;
+       let sfb = metaplex_token_metadata::state::Metadata::from_account_info(&ctx.accounts.token_metadata_account)?.data.seller_fee_basis_points;
+       let sfb_cut = sfb as u64 * taker_amount / 10000;
+       taker_amount = taker_amount - (market_cut + sfb_cut);
+
+        if *ctx.accounts.tokenrent.key != ctx.accounts.data_acc.rent {
+            return Err(ProgramError::Custom(0x1));
+        }
+        
         let transfer_ix = anchor_lang::solana_program::system_instruction::transfer(
             ctx.accounts.offer_taker.key,
             ctx.accounts.tokenrent.key,
@@ -76,13 +105,10 @@ pub mod quidproquo {
             ],
         )?;
 
-
-
-
         let transfer_ix = anchor_lang::solana_program::system_instruction::transfer(
             ctx.accounts.offer_taker.key,
             ctx.accounts.offer_maker.key,
-             ctx.accounts.offer.taker_amount
+             taker_amount,
         );
 
         anchor_lang::solana_program::program::invoke(
@@ -93,44 +119,174 @@ pub mod quidproquo {
                 ctx.accounts.offer.to_account_info(),
             ],
         )?;
+        if *ctx.accounts.market_maker.key != ctx.accounts.data_acc.market_place {
+            return Err(ProgramError::Custom(0x1));
+        }
+        
+        let transfer_ix = anchor_lang::solana_program::system_instruction::transfer(
+            ctx.accounts.offer_taker.key,
+            ctx.accounts.market_maker.key,
+             market_cut,
+        );
 
-
-
-        // Transfer the maker's tokens (the ones they escrowed) to the taker.
-        anchor_spl::token::transfer(
-            CpiContext::new_with_signer(
-                ctx.accounts.token_program.to_account_info(),
-                anchor_spl::token::Transfer {
-                    from: ctx.accounts.escrowed_maker_tokens.to_account_info(),
-                    to: ctx.accounts.offer_takers_maker_tokens.to_account_info(),
-                    // Cute trick: the escrowed_maker_tokens is its own
-                    // authority/owner (and a PDA, so our program can sign for
-                    // it just below)
-                    authority: ctx.accounts.escrowed_maker_tokens.to_account_info(),
-                },
-                &[&[
-                    ctx.accounts.offer.key().as_ref(),
-                    &[ctx.accounts.offer.escrowed_maker_tokens_bump],
-                ]],
-            ),
-            // The amount here is just the entire balance of the escrow account.
-          1,
+        anchor_lang::solana_program::program::invoke(
+            &transfer_ix,
+            &[
+                ctx.accounts.offer_taker.to_account_info(),
+                ctx.accounts.market_maker.to_account_info(),
+                ctx.accounts.offer.to_account_info(),
+            ],
         )?;
 
-        //Finally, close the escrow account and refund the maker (they paid for
-        // its rent-exemption).
-        anchor_spl::token::close_account(CpiContext::new_with_signer(
-            ctx.accounts.token_program.to_account_info(),
-            anchor_spl::token::CloseAccount {
-                account: ctx.accounts.escrowed_maker_tokens.to_account_info(),
-                destination: ctx.accounts.tokenrent.to_account_info(),
-                authority: ctx.accounts.escrowed_maker_tokens.to_account_info(),
-            },
-            &[&[
-                ctx.accounts.offer.key().as_ref(),
-                &[ctx.accounts.offer.escrowed_maker_tokens_bump],
-            ]],
-        ))
+        if sfb_cut > 0 {    
+          // stick those CPIs in here
+            if let Some(x) = metaplex_token_metadata::state::Metadata::from_account_info(&ctx.accounts.token_metadata_account)?.data.creators {
+                let mut y = 0;
+
+            for i in x {
+                    if y == 0 {
+                        if i.address != *ctx.accounts.creator0.key {
+                            return Err(ProgramError::Custom(0x1));
+                        }
+                        let transfer_ix = anchor_lang::solana_program::system_instruction::transfer(
+                            ctx.accounts.offer_taker.key,
+                            ctx.accounts.creator0.key,
+                            sfb_cut as u64 * i.share as u64 / 100,
+                        );
+                        
+                        anchor_lang::solana_program::program::invoke(
+                            &transfer_ix,
+                            &[
+                                ctx.accounts.offer_taker.to_account_info(),
+                                ctx.accounts.creator0.to_account_info(),
+                                ctx.accounts.offer.to_account_info(),
+                            ],
+                        )?;
+                    }
+                    else if y == 1 {
+                        if i.address != *ctx.accounts.creator1.key {
+                            return Err(ProgramError::Custom(0x1));
+                        }
+                        let transfer_ix = anchor_lang::solana_program::system_instruction::transfer(
+                            ctx.accounts.offer_taker.key,
+                            ctx.accounts.creator1.key,
+                            sfb_cut as u64 * i.share as u64 / 100,
+                        );
+                        
+                        anchor_lang::solana_program::program::invoke(
+                            &transfer_ix,
+                            &[
+                                ctx.accounts.offer_taker.to_account_info(),
+                                ctx.accounts.creator1.to_account_info(),
+                                ctx.accounts.offer.to_account_info(),
+                            ],
+                        )?;
+                    }
+                    else if y == 2 {
+                        if i.address != *ctx.accounts.creator2.key {
+                            return Err(ProgramError::Custom(0x1));
+                        }
+                        let transfer_ix = anchor_lang::solana_program::system_instruction::transfer(
+                            ctx.accounts.offer_taker.key,
+                            ctx.accounts.creator2.key,
+                            sfb_cut as u64 * i.share as u64 / 100,
+                        );
+                        
+                        anchor_lang::solana_program::program::invoke(
+                            &transfer_ix,
+                            &[
+                                ctx.accounts.offer_taker.to_account_info(),
+                                ctx.accounts.creator2.to_account_info(),
+                                ctx.accounts.offer.to_account_info(),
+                            ],
+                        )?;
+                    }
+                    else if y == 3 {
+                        if i.address != *ctx.accounts.creator3.key {
+                            return Err(ProgramError::Custom(0x1));
+                        }
+                        let transfer_ix = anchor_lang::solana_program::system_instruction::transfer(
+                            ctx.accounts.offer_taker.key,
+                            ctx.accounts.creator3.key,
+                            sfb_cut as u64 * i.share as u64 / 100,
+                        );
+                        
+                        anchor_lang::solana_program::program::invoke(
+                            &transfer_ix,
+                            &[
+                                ctx.accounts.offer_taker.to_account_info(),
+                                ctx.accounts.creator3.to_account_info(),
+                                ctx.accounts.offer.to_account_info(),
+                            ],
+                        )?;
+                    }
+                    else if y == 4 {
+                        if i.address != *ctx.accounts.creator1.key {
+                            return Err(ProgramError::Custom(0x1));
+                        }
+                        let transfer_ix = anchor_lang::solana_program::system_instruction::transfer(
+                            ctx.accounts.offer_taker.key,
+                            ctx.accounts.creator4.key,
+                            sfb_cut as u64 * i.share as u64 / 100,
+                        );
+                        
+                        anchor_lang::solana_program::program::invoke(
+                            &transfer_ix,
+                            &[
+                                ctx.accounts.offer_taker.to_account_info(),
+                                ctx.accounts.creator4.to_account_info(),
+                                ctx.accounts.offer.to_account_info(),
+                            ],
+                        )?;
+                    }
+                    y = y + 1;
+                    msg!("address {}", i.address);
+                    msg!("share in pc {}", i.share);
+            }
+
+            }
+
+        }
+
+        // Transfer the maker's tokens (the ones they escrowed) to the taker.
+            anchor_spl::token::transfer(
+                            CpiContext::new_with_signer(
+                                ctx.accounts.token_program.to_account_info(),
+                                anchor_spl::token::Transfer {
+                                    from: ctx.accounts.escrowed_maker_tokens.to_account_info(),
+                                    to: ctx.accounts.offer_takers_maker_tokens.to_account_info(),
+                                    // Cute trick: the escrowed_maker_tokens is its own
+                                    // authority/owner (and a PDA, so our program can sign for
+                                    // it just below)
+                                    authority: ctx.accounts.escrowed_maker_tokens.to_account_info(),
+                                },
+                                &[&[
+                                    ctx.accounts.offer.key().as_ref(),
+                                    &[ctx.accounts.offer.escrowed_maker_tokens_bump],
+                                ]],
+                            ),
+                            // The amount here is just the entire balance of the escrow account.
+                          1,
+            )?;
+            msg!("About to close account");
+            //Finally, close the escrow account and refund the maker (they paid for
+            // its rent-exemption).
+            anchor_spl::token::close_account(CpiContext::new_with_signer(
+                            ctx.accounts.token_program.to_account_info(),
+                            anchor_spl::token::CloseAccount {
+                                account: ctx.accounts.escrowed_maker_tokens.to_account_info(),
+                                destination: ctx.accounts.tokenrent.to_account_info(),
+                                authority: ctx.accounts.escrowed_maker_tokens.to_account_info(),
+                            },
+                            &[&[
+                                ctx.accounts.offer.key().as_ref(),
+                                &[ctx.accounts.offer.escrowed_maker_tokens_bump],
+                            ]],
+            ))?;
+            msg!("Function End");
+            Ok(())
+ 
+
     }
 
     pub fn cancel(ctx: Context<Cancel>, _offer_bump:u8) -> ProgramResult {
@@ -188,6 +344,18 @@ pub mod quidproquo {
 }
 
 #[account]
+pub struct Data {
+
+    pub market_place: Pubkey,
+    
+    pub market_place_cut: u64,
+
+    pub rent: Pubkey,
+
+    pub rent_cut: u64,
+}
+
+#[account]
 pub struct Offer {
     // We store the offer maker's key so that they can cancel the offer (we need
     // to know who should sign).
@@ -199,6 +367,29 @@ pub struct Offer {
     // by the `Offer` account's address. Storing the corresponding bump here
     // means the client doesn't have to keep passing it.
     pub escrowed_maker_tokens_bump: u8,
+}
+
+#[derive(Accounts)]
+#[instruction(data_bump: u8)]
+
+pub struct Initialize<'info> {
+    #[account(init, payer=payer, seeds = [b"data".as_ref()], bump = data_bump, space = 8 + 32 + 8 + 32 + 8)]
+    pub data_acc: Account<'info, Data>,
+
+    #[account(mut)]
+    pub payer: Signer<'info>,
+
+    #[account()]
+
+    pub beneficiary: AccountInfo<'info>,
+
+    #[account()]
+    pub rent_account: AccountInfo<'info>,
+
+    pub token_program: Program<'info, Token>,
+    pub system_program: Program<'info, System>,
+    pub rent: Sysvar<'info, Rent>,
+
 }
 
 #[derive(Accounts)]
@@ -228,6 +419,8 @@ pub struct Make<'info> {
     pub escrowed_maker_tokens: Account<'info, TokenAccount>,
 
     pub maker_mint: Account<'info, Mint>,
+
+    pub data_acc: Account<'info, Data>,
   
     pub token_program: Program<'info, Token>,
     pub system_program: Program<'info, System>,
@@ -250,14 +443,14 @@ pub struct Accept<'info> {
         // anymore) and send its rent back to the offer_maker
         close = offer_maker
     )]
-    pub offer: Account<'info, Offer>,
+    pub offer: Box<Account<'info, Offer>>,
 
     #[account(
         mut,
         seeds = [offer.key().as_ref()],
         bump = offer.escrowed_maker_tokens_bump
     )]
-    pub escrowed_maker_tokens: Account<'info, TokenAccount>,
+    pub escrowed_maker_tokens: Box<Account<'info, TokenAccount>>,
 
     pub maker_mint: Account<'info, Mint>,
 
@@ -265,17 +458,44 @@ pub struct Accept<'info> {
     pub offer_maker: AccountInfo<'info>,
     pub offer_taker: Signer<'info>,
 
+ 
     #[account(init_if_needed, payer = offer_taker, associated_token::mint = maker_mint, associated_token::authority = offer_taker)]
     pub offer_takers_maker_tokens: Account<'info, TokenAccount>,
 
     pub associated_token_program: Program<'info, AssociatedToken>,
+    
+    #[account()]
+    pub token_metadata_account: AccountInfo<'info>,
+    
+    #[account()]
+    pub token_metadata_program: AccountInfo<'info>,
+
+    #[account(mut)]
+    pub market_maker: AccountInfo<'info>,
 
     pub token_program: Program<'info, Token>,
     pub system_program: Program<'info, System>,
     pub rent: Sysvar<'info, Rent>,
 
     #[account(mut)]
-    pub tokenrent: AccountInfo<'info>
+    pub tokenrent: AccountInfo<'info>,
+
+    pub data_acc: Account<'info, Data>,
+
+     #[account(mut)]
+    pub creator0: AccountInfo<'info>,
+
+    #[account(mut)]
+    pub creator1: AccountInfo<'info>,
+
+    #[account(mut)]
+    pub creator2: AccountInfo<'info>,
+
+    #[account(mut)]
+    pub creator3: AccountInfo<'info>,
+
+    #[account(mut)]
+    pub creator4: AccountInfo<'info>
 }
 
 #[derive(Accounts)]
@@ -316,6 +536,8 @@ pub struct Cancel<'info> {
     pub token_program: Program<'info, Token>,
     pub system_program: Program<'info, System>,
     pub rent: Sysvar<'info, Rent>,
+
+    pub data_acc: Account<'info, Data>,
 
     #[account(mut)]
     pub tokenrent: AccountInfo<'info>
