fc196a6a2e0fcdb26598d0f5bee8677f9f282521
Aditya Angadi <adityaangadi96@gmail.com>
2021-12-17 15:33:54 +0000

    HOPEFULLY LAST COMMIT
    Signed-off-by: Aditya Angadi <adityaangadi96@gmail.com>

--- programs/quidproquo/src/lib.rs
+++ programs/quidproquo/src/lib.rs
@@ -6,7 +6,7 @@ use anchor_spl::{
 
 use metaplex_token_metadata;
 
-declare_id!("DdJumnZ17cGxxYt9RbRoCPBbXK3F8BKyc9u1Lv8kGwJY");
+declare_id!("BVf2Gmc9zX5HUXGuLjDkTrFYjZmNz7Z2UHohMqQoa5rk");
 
 #[program]
 pub mod quidproquo {
@@ -16,14 +16,12 @@ pub mod quidproquo {
         ctx: Context<Initialize>,
         _data_bump: u8,
         mk_cut: u64,
-        rent_cut: u64
     ) -> ProgramResult {
         let data_acc = &mut ctx.accounts.data_acc;
         data_acc.market_place = ctx.accounts.beneficiary.key();
-        data_acc.ata_rent = ctx.accounts.rent_account.key();
         data_acc.pda_rent = ctx.accounts.pda_rent_account.key();
         data_acc.market_place_cut = mk_cut;
-        data_acc.rent_cut = rent_cut;
+     
         Ok(())
     }
 
@@ -38,7 +36,7 @@ pub mod quidproquo {
     ) -> ProgramResult {
         // Store some state about the offer being made. We'll need this later if
         // the offer gets accepted or cancelled.
-        msg!("Function start");
+       
         let offer = &mut ctx.accounts.offer;
         offer.maker = ctx.accounts.offer_maker.key();
         offer.mint = ctx.accounts.nft_mint.key();
@@ -47,13 +45,14 @@ pub mod quidproquo {
         offer.offer_expired = false;
 
         let clock = &ctx.accounts.clock;
-        msg!("timestamp is {}", clock.unix_timestamp);
+
         if clock.unix_timestamp >= offer_valid {
             return Err(ProgramError::Custom(0x4));
         }
-        offer.offer_valid_till = Some(offer_valid);
 
-        msg!("Here");
+        offer.offer_valid_till = offer_valid;
+
+  
         let transfer_ix = anchor_lang::solana_program::system_instruction::transfer(
             ctx.accounts.offer_maker.key,
             ctx.accounts.offer.to_account_info().key,
@@ -64,7 +63,6 @@ pub mod quidproquo {
             &transfer_ix,
             &[
                 ctx.accounts.offer_maker.to_account_info(),
-                ctx.accounts.tokenrent.to_account_info(),
                 ctx.accounts.offer.to_account_info(),
             ],
         )?;
@@ -79,6 +77,16 @@ pub mod quidproquo {
     pub fn accept(ctx: Context<Accept>, offer_bump:u8, _stick_bump:u8, _data_bump:u8, offer_valid:i64) -> ProgramResult {
 
         let offer = &mut ctx.accounts.offer;
+
+        if offer.offer_expired == true {
+            return Err(ProgramError::Custom(0x6));
+        }
+
+        let clock = &ctx.accounts.clock;
+        if clock.unix_timestamp >= ctx.accounts.offer.offer_valid_till {
+            return Err(ProgramError::Custom(0x4));
+        }
+
         offer.offer_expired = true;
 
         let stick = &mut ctx.accounts.stick;
@@ -96,13 +104,8 @@ pub mod quidproquo {
        let sfb_cut = sfb as u64 * taker_amount / 10000;
        taker_amount = taker_amount - (market_cut + sfb_cut);
         
-       let clock = &ctx.accounts.clock;
-       if clock.unix_timestamp >= ctx.accounts.offer.offer_valid_till.unwrap() {
-           return Err(ProgramError::Custom(0x4));
-       }
-        if *ctx.accounts.tokenrent.key != ctx.accounts.data_acc.ata_rent {
-            return Err(ProgramError::Custom(0x1));
-        }
+
+
 
         if *ctx.accounts.offer_maker.key != ctx.accounts.offer.maker {
             return Err(ProgramError::Custom(0x11));
@@ -121,14 +124,14 @@ pub mod quidproquo {
             1,
         )?;
 
-        msg!("About to close account");
+   
         //Finally, close the escrow account and refund the maker (they paid for
         // its rent-exemption).
         anchor_spl::token::close_account(CpiContext::new(
                         ctx.accounts.token_program.to_account_info(),
                         anchor_spl::token::CloseAccount {
                             account: ctx.accounts.offer_takers_nft_token.to_account_info(),
-                            destination: ctx.accounts.tokenrent.to_account_info(),
+                            destination: ctx.accounts.offer_maker.to_account_info(),
                             authority: ctx.accounts.offer_taker.to_account_info(),
                         }
         ))?;
@@ -210,34 +213,29 @@ pub mod quidproquo {
             }
 
         }
-        msg!("Doing transfer");
-        msg!("from: {}", ctx.accounts.offer_takers_nft_token.to_account_info().key);
-        msg!("to: {}", ctx.accounts.offer_makers_nft_account.to_account_info().key);
-        msg!("auth: {}", ctx.accounts.offer_taker.to_account_info().key);
-
-
-        msg!("Function End");
 
         Ok(())
  
     }
 
     pub fn cancel(ctx: Context<Cancel>, _offer_bump:u8, offer_valid:i64) -> ProgramResult {
-        //// TODO implement clock check
+
         let clock = &ctx.accounts.clock;
-        if clock.unix_timestamp <= ctx.accounts.offer.offer_valid_till.unwrap() {
+        let offer = &mut ctx.accounts.offer;
+        if offer.offer_expired == true {
             return Err(ProgramError::Custom(0x4));
         }
-        let offer = &mut ctx.accounts.offer;
+      
         offer.offer_expired = true;
         let temp =  ctx.accounts.offer.offer_amount;
         **ctx.accounts.offer.to_account_info().try_borrow_mut_lamports()? -= temp;
         **ctx.accounts.offer_maker.to_account_info().try_borrow_mut_lamports()? += temp;
 
         Ok(())
+    
     }
 
-    pub fn close_offer_pda(ctx: Context<CloseOfferPDA>, _offer_bump:u8, offer_valid:i64) -> ProgramResult {
+    pub fn close_offer_pda(ctx: Context<CloseOfferPDA>, _offer_bump:u8, data_bump:u8, offer_valid:i64) -> ProgramResult {
         if ctx.accounts.data_acc.pda_rent != *ctx.accounts.pda_rent.key {
             return Err(ProgramError::Custom(0x6));
         }
@@ -248,14 +246,12 @@ pub mod quidproquo {
         Ok(())
     }
 
-    pub fn close_stick_pda(ctx: Context<CloseStickPDA>, stick_bump:u8, offer_valid:i64) -> ProgramResult {
+    pub fn close_stick_pda(ctx: Context<CloseStickPDA>, stick_bump:u8, data_bump:u8, offer_valid:i64) -> ProgramResult {
+
         if ctx.accounts.data_acc.pda_rent != *ctx.accounts.pda_rent.key {
             return Err(ProgramError::Custom(0x6));
         }
-        let expired =  ctx.accounts.stick.offer_expired;
-        if !expired {
-            return Err(ProgramError::Custom(0x7));
-        }
+
         Ok(())
     }
 
@@ -266,9 +262,7 @@ pub mod quidproquo {
 pub struct Data {
     pub market_place: Pubkey,
     pub market_place_cut: u64,
-    pub ata_rent: Pubkey,
     pub pda_rent: Pubkey,
-    pub rent_cut: u64,
 }
 
 #[account]
@@ -279,7 +273,7 @@ pub struct Offer {
     pub mint: Pubkey,
     pub offer_made_for: Pubkey,
     pub offer_amount: u64,
-    pub offer_valid_till: Option<i64>,
+    pub offer_valid_till: i64,
     pub offer_expired: bool
 }
 
@@ -300,24 +294,14 @@ pub struct Stick {
 pub struct Initialize<'info> {
     #[account(init, payer=payer, seeds = [b"data".as_ref()], bump = data_bump, space = 8 + 32 + 8 + 32 + 64 + 32 + 64 + 500 + 8)]
     pub data_acc: Account<'info, Data>,
-
     #[account(mut)]
     pub payer: Signer<'info>,
-
     #[account()]
-
     pub beneficiary: AccountInfo<'info>,
-
-    #[account()]
-    pub rent_account: AccountInfo<'info>,
-
     pub token_program: Program<'info, Token>,
     pub system_program: Program<'info, System>,
     pub rent: Sysvar<'info, Rent>,
-
-    #[account(mut)]
-    pub tokenrent: AccountInfo<'info>,
-    #[account(mut)]
+    
     pub pda_rent_account: AccountInfo<'info>
 
 }
@@ -325,7 +309,8 @@ pub struct Initialize<'info> {
 #[derive(Accounts)]
 #[instruction(offer_bump:u8, offer_valid:i64, nft_offer_amount:u64 )]
 pub struct Make<'info> {
-    #[account(init, payer = offer_maker, seeds = [offer_maker.to_account_info().key.as_ref(), nft_mint.to_account_info().key.as_ref(), offer_valid.to_be_bytes().as_ref()], bump = offer_bump,  space = 1050)]
+    #[account(init, payer = offer_maker, seeds = [offer_maker.to_account_info().key.as_ref(), nft_mint.to_account_info().key.as_ref(), offer_valid.to_be_bytes().as_ref()], bump = offer_bump,
+    space = 850)]
     pub offer: Account<'info, Offer>,
 
     #[account(mut)]
@@ -343,9 +328,6 @@ pub struct Make<'info> {
     pub associated_token_program: Program<'info, AssociatedToken>,
     pub rent: Sysvar<'info, Rent>,
     pub clock: Sysvar<'info, Clock>,
-
-    #[account(mut)]
-    pub tokenrent: AccountInfo<'info>
 }
 
 #[derive(Accounts)]
@@ -363,7 +345,8 @@ pub struct Accept<'info> {
 
     #[account(init, payer = offer_taker, 
         seeds = [offer_maker.to_account_info().key.as_ref(), nft_mint.to_account_info().key.as_ref(), offer_taker.to_account_info().key.as_ref(), offer_valid.to_be_bytes().as_ref()], 
-        bump = stick_bump,  space = 1050)]
+        bump = stick_bump,
+        space = 1050)]
     pub stick: Account<'info, Stick>,
 
     pub nft_mint: Box<Account<'info, Mint>>,
@@ -396,9 +379,6 @@ pub struct Accept<'info> {
     pub system_program: Program<'info, System>,
     pub rent: Sysvar<'info, Rent>,
     pub clock: Sysvar<'info, Clock>,
-
-    #[account(mut)]
-    pub tokenrent: AccountInfo<'info>,
 
     #[account(seeds = [b"data".as_ref()], bump = data_bump)]
     pub data_acc: Account<'info, Data>,
@@ -447,14 +427,11 @@ pub struct Cancel<'info> {
     pub data_acc: Account<'info, Data>,
 
     pub clock: Sysvar<'info, Clock>,
-
-    #[account(mut)]
-    pub tokenrent: AccountInfo<'info>
 }
 
 
 #[derive(Accounts)]
-#[instruction(offer_bump:u8, offer_valid:i64)]
+#[instruction(offer_bump:u8, data_bump:u8, offer_valid:i64)]
 pub struct CloseOfferPDA<'info> {
     #[account(
         mut,
@@ -475,6 +452,7 @@ pub struct CloseOfferPDA<'info> {
     // pub token_program: Program<'info, Token>,
     pub system_program: Program<'info, System>,
     pub rent: Sysvar<'info, Rent>,
+    #[account(seeds = [b"data".as_ref()], bump = data_bump)]
     pub data_acc: Account<'info, Data>,
     // pub clock: Sysvar<'info, Clock>,
     #[account(mut)]
@@ -482,7 +460,7 @@ pub struct CloseOfferPDA<'info> {
 }
 
 #[derive(Accounts)]
-#[instruction(stick_bump:u8, offer_valid:i64)]
+#[instruction(stick_bump:u8, data_bump:u8, offer_valid:i64)]
 pub struct CloseStickPDA<'info> {
     #[account(
         mut,
@@ -502,6 +480,8 @@ pub struct CloseStickPDA<'info> {
     // pub token_program: Program<'info, Token>,
     pub system_program: Program<'info, System>,
     pub rent: Sysvar<'info, Rent>,
+
+    #[account(seeds = [b"data".as_ref()], bump = data_bump)]
     pub data_acc: Account<'info, Data>,
     // pub clock: Sysvar<'info, Clock>,
     #[account(mut)]
